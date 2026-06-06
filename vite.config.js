c6881b80de9cc48a57a425ce54c6a875d06f687d
salveci2022 <seu@email.com>
2026-06-05 23:22:42 -0300

    fix vite render

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,16 @@ export default defineConfig({
       },
     },
   },
-  build: { outDir: 'dist', sourcemap: false },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts'],
+        },
+      },
+    },
+  },
 })
